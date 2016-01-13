467519735cf6b67ff38c7ee41ab68cad7915501a
okumin <mail@okumin.com>
2016-01-14 03:56:30 +0900

    Add a benchmark for AkkaTimeoutFailurewallBenchmark

---
+++ failurewall-benchmark/src/test/scala/failurewall/timeout/AkkaTimeoutFailurewallBenchmark.scala
@@ -0,0 +1,14 @@
+package failurewall.timeout
+
+import failurewall.Failurewall
+import failurewall.test.{ActorSystemSetup, FailurewallBenchmark}
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration._
+
+class AkkaTimeoutFailurewallBenchmark extends FailurewallBenchmark with ActorSystemSetup {
+  override protected[this] val failurewall: Failurewall[Int, Int] = AkkaTimeoutFailurewall(
+    duration = 10.seconds,
+    scheduler = system.scheduler,
+    executor = global
+  )(())
+}
