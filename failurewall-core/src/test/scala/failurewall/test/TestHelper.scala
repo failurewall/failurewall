58aef74e247c847dfd3cccba79e0799aba69f95e
okumin <mail@okumin.com>
2016-01-14 04:09:10 +0900

    Change default duration of TestHelper

--- failurewall-core/src/test/scala/failurewall/test/TestHelper.scala
+++ failurewall-core/src/test/scala/failurewall/test/TestHelper.scala
@@ -6,9 +6,9 @@ import scala.concurrent.{Await, Future}
 import scala.util.Try
 
 object TestHelper {
-  def await[A](future: Future[A]): Try[A] = Try(Await.result(future, 50.seconds))
+  def await[A](future: Future[A]): Try[A] = Try(Await.result(future, 500.seconds))
 
-  def awaitAssert(condition: => Boolean, until: FiniteDuration = 50.millis): Unit = {
+  def awaitAssert(condition: => Boolean, until: FiniteDuration = 500.millis): Unit = {
     val trial = (1 to 10).foldLeft(condition) {
       case (true, _) => true
       case (false, _) =>
@@ -18,7 +18,7 @@ object TestHelper {
     if (!trial) assert(condition)
   }
 
-  def assertWhile(condition: => Boolean, until: FiniteDuration = 50.millis): Unit = {
+  def assertWhile(condition: => Boolean, until: FiniteDuration = 300.millis): Unit = {
     val trial = (1 to 10).foldLeft(condition) {
       case (true, _) =>
         TimeUnit.NANOSECONDS.sleep(until.toNanos / 10)
