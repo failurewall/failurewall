066b5e6bce19f9d1165c82a88283be3845a765e4
okumin <mail@okumin.com>
2016-01-14 03:19:06 +0900

    Add a benchmark for AkkaRetryFailurewall

---
+++ failurewall-benchmark/src/test/scala/failurewall/retry/AkkaRetryFailurewallBenchmark.scala
@@ -0,0 +1,15 @@
+package failurewall.retry
+
+import failurewall.Failurewall
+import failurewall.test.{ActorSystemSetup, FailurewallBenchmark}
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration._
+
+class AkkaRetryFailurewallBenchmark extends FailurewallBenchmark with ActorSystemSetup {
+  override protected[this] val failurewall: Failurewall[Int, Int] = AkkaRetryFailurewall(
+    maxTrialTimes = 5,
+    strategy = ExponentialBackoffStrategy(minBackoff = 10.millis, maxBackoff = 100.millis),
+    scheduler = system.scheduler,
+    executor = global
+  )
+}
